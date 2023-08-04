5b05b509b98159909c1dec821066056d23c18be2
Sid55a <siddharth55x@gmail.com>
2023-08-05 00:10:39 +0530

    error fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
 import Popular from "./popular/Popular";
-import TopRated from "./topRated/TopRated ";
+import TopRated from "./topRated/TopRated";
 
 function Home() {
   return (

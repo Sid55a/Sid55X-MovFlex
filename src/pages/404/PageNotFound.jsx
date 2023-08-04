d2815a0574d667d4f8f53c9e7e4cd929f6f06e53
Sid55a <siddharth55x@gmail.com>
2023-08-04 10:44:44 +0530

    error 404 page added

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-function PageNotFound() {
-  return <div>pageNotFound</div>;
-}
+import "./style.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
 
 export default PageNotFound;
